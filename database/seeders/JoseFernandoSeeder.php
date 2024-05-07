35c9e9ec537b3d8f55728af9725ced96cc0f08a3
lizmar03 <lizmargomes2003@gmail.com>
2024-05-07 11:41:33 -0400

    acomodo detalle en pdf

--- database/seeders/JoseFernandoSeeder.php
+++ database/seeders/JoseFernandoSeeder.php
@@ -50,12 +50,15 @@ class JoseFernandoSeeder extends Seeder
             'password' => ('josefernando10'),
         ]);
 
-        $rol = Role::create(['name'=>'Administrador']);
+        // $rol = Role::create(['name'=>'Administrador']);
 
-        $permisos = Permission::pluck('id', 'id')->all();
+        // $permisos = Permission::pluck('id', 'id')->all();
 
-        $rol->syncPermissions($permisos);
+        // $rol->syncPermissions($permisos);
 
-        $usuario->assignRole([$rol->id]);
+        $usuario->assignRole('Administrador');
+
+
+
     }
 }
