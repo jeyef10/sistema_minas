c65e16b206e11a359f6f0a3af1dc351e87c6addd
lizmar03 <lizmargomes2003@gmail.com>
2024-05-10 16:48:45 -0400

    implementacion de dashbord

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,24 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-// use App\Models\Sede;
-// use App\Models\Division;
-// use App\Models\Cargo;
-// use App\Models\Marca;
-// use App\Models\Modelo;
-// use App\Models\Persona;
-// use App\Models\TipoPeriferico;
-// use App\Models\Perifericos;
-// use App\Models\Sistema;
-// use App\Models\Equipos;
-// use App\Models\Asignar;
+use App\Models\Solicitante;
+use App\Models\Recaudos;
+use App\Models\Comisionados;
+use App\Models\Minerales;
+use App\Models\Regalia;
+use App\Models\Plazos;
+
 
 class homeController extends Controller
 {
     //
     public function index(){
 
-        return view('home.inicio');
+        $solicitantes = Solicitante::all();
+        $count_solicitante = DB::table('solicitantes')
+        ->count();
+
+
+        $recaudos = Recaudos::all();
+        $count_recaudo= DB::table('recaudos')
+        ->count();
+
+        $comisionados = Comisionados::all();
+        $count_comisionado= DB::table('comisionados')
+        ->count();
+
+        $minerales= Minerales::all();
+        $count_mineral= DB::table('minerales')
+        ->count();
+
+        $regalias = Regalia::all();
+        $count_regalia= DB::table('regalias')
+        ->count();
+        
+        $plazos = Plazos::all();
+        $count_plazo= DB::table('plazos')
+        ->count();
+
+
+
+        return view('home.inicio' , compact('count_solicitante',  'count_recaudo','count_comisionado', 'count_mineral','count_regalia', 'count_plazo'  ) , [
+        'count' => $count_solicitante, $count_recaudo,  $count_comisionado,  $count_mineral,   $count_regalia, $count_plazo
+
+    ]); ;
 
     }
 
--- resources/views/home/inicio.blade.php
+++ resources/views/home/inicio.blade.php
@@ -5,94 +5,134 @@
 @section('contenido')
 
                                                         <!-- Container Fluid Contenido Dentro del Cuadro con Fondo Blanco-->
-    <div class="container-fluid" id="container-wrapper">
+  <div class="container-fluid" id="container-wrapper">
         <div class="d-sm-flex align-items-center justify-content-between mb-4"></div>
 
         <div class="row mb-3">
 
-                                                                <!-- Earnings (Monthly) Card Example Primer Cuarito-->
+                                                                <!-- dasboard solicitante-->
           <div class="col-xl-3 col-md-6 mb-4">
             <div class="card h-100">
               <div class="card-body">
                 <div class="row align-items-center">
                   <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-uppercase mb-1">Earnings (Monthly)</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                    <div class="mt-2 mb-0 text-muted text-xs">
-                      <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                      <span>Since last month</span>
-                    </div>
+                    <div class="text-xs font-weight-bold text-uppercase mb-1">Solicitante</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ ($count_solicitante) }}</div>
+                    
                   </div>
                   <div class="col-auto">
-                    <i class="fas fa-calendar fa-2x text-primary"></i>
+                  <i class="fas fa-users fa-2x text-info"></i>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-                                                                <!-- Earnings (Annual) Card Example Segundo Cuadrito-->
+                                                                <!--dasboard recaudo-->
           <div class="col-xl-3 col-md-6 mb-4">
             <div class="card h-100">
               <div class="card-body">
                 <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-uppercase mb-1">Sales</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">650</div>
-                    <div class="mt-2 mb-0 text-muted text-xs">
-                      <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
-                      <span>Since last years</span>
+                    <div class="text-xs font-weight-bold text-uppercase mb-1">Recaudo</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">{{ ($count_recaudo) }}</div>
+
                     </div>
-                  </div>
                   <div class="col-auto">
-                    <i class="fas fa-shopping-cart fa-2x text-success"></i>
+                  <i class="fa fa-folder-open fa-2x text-info" ></i>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-                                                                  <!-- New User Card Example Tercer Cuadrito-->
+        
+                                                                  <!-- dasboard comisionados-->
           <div class="col-xl-3 col-md-6 mb-4">
             <div class="card h-100">
               <div class="card-body">
                 <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-uppercase mb-1">New User</div>
-                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">366</div>
-                    <div class="mt-2 mb-0 text-muted text-xs">
-                      <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
-                      <span>Since last month</span>
-                    </div>
+                    <div class="text-xs font-weight-bold text-uppercase mb-1">Comisionado</div>
+                     <div class="h5 mb-0 font-weight-bold text-gray-800">{{ ($count_comisionado) }}</div>  
                   </div>
+
                   <div class="col-auto">
-                    <i class="fas fa-users fa-2x text-info"></i>
+                    <i class=" fas fa-user-tie fa-2x text-info"></i>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-                                                                <!-- Pending Requests Card Example Cuarto Cuadrito -->
+                                                                <!-- dasboard minerales -->
           <div class="col-xl-3 col-md-6 mb-4">
             <div class="card h-100">
               <div class="card-body">
-                <div class="row no-gutters align-items-center">
+                <div class="row align-items-center">
                   <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-uppercase mb-1">Pending Requests</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                    <div class="mt-2 mb-0 text-muted text-xs">
-                      <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
-                      <span>Since yesterday</span>
-                    </div>
+                    <div class="text-xs font-weight-bold text-uppercase mb-1">Mineral</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ ($count_mineral) }}</div>    
                   </div>
+
                   <div class="col-auto">
-                    <i class="fas fa-comments fa-2x text-warning"></i>
+                  <i class="fas fa-mountain fa-2x text-info"></i>
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
+
+          
+                                                                    <!-- dasboard Tasa de ragalias -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card h-100">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-uppercase mb-1">Tasa de Regalias</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ ($count_regalia) }}</div>
+                 
+                  </div>
+                  <div class="col-auto">
+                    <i class="far fa-money-bill-alt fa-2x text-info"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+                                                               <!-- dasboard Plazos de Vigencia-->
+             <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card h-100">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-uppercase mb-1">Plazos de Vigencia</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ ($count_plazo) }}</div>
+                 
+                  </div>
+                  <div class="col-auto">
+                    <i class="far fa-calendar-alt fa-2x text-warning"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> 
+
+
+
+
+
+
+      </div>
+  </div>
+
+                                                                
+            
+
                                                                   <!-- Area Chart Cuadro de Estadisticas -->
-          <div class="col-xl-12 col-lg-7">
+          <!-- <div class="col-xl-12 col-lg-7">
             <div class="card mb-4">
               <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Monthly Recap Report</h6>
@@ -117,8 +157,7 @@
                 </div>
               </div>
             </div>
-          </div>
-      </div>
+          
 
 @endsection
 
